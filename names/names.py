1f95645c7b4841233dba5088a3de8283cb06dab8
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-07-26 14:19:29 -0400

    Clean up + logs

--- names/names.py
+++ names/names.py
@@ -10,8 +10,8 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-"""
-duplicates = []
+
+"""duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
@@ -19,15 +19,15 @@ for name_1 in names_1:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-"""
+print (f"runtime: {end_time - start_time} seconds")"""
+
 
 dupes = []
 hashtable = dict.fromkeys(names_1, 0)
-for i in range(len(names_2) -1):
+for i in range(len(names_2) - 1):
     if hashtable.get(names_2[i]) == 0:
         dupes.append(names_2[i])
 
 end_time = time.time()
-print (f"{len(dupes)} duplicates:\n\n{', '.join(dupes)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(dupes)} duplicates:\n\n{', '.join(dupes)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,17 +1,17 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
+    def append(self, item):
         self.storage[self.current] = item
         if self.current < self.capacity - 1:
             self.current += 1
         else:
             self.current = 0
 
-  def get(self):
+    def get(self):
         list = []
         for value in self.storage:
             if value is not None:
